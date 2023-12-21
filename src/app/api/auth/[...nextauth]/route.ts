4f6423f5186378a6761ff9380bc73d93576e6c1f
Arjun Nair <arjun@nairsnet.com>
2023-12-21 14:10:43 -0500

    add secret?

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,12 +3,13 @@ import GoogleProvider from "next-auth/providers/google";
 
 
 const handler = NextAuth({
+    secret: process.env.SECRET,
     providers: [
         GoogleProvider({
-            clientId:process.env.GOOGLE_CLIENT_ID ?? "",
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         })
     ]
 });
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST }
--- src/app/components/AppBar.tsx
+++ src/app/components/AppBar.tsx
@@ -3,10 +3,10 @@ import SignInButton from './SignInButton'
 
 type Props = {}
 
-export default function AppBar({}: Props) {
-  return (
-    <header className='flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow'>
-    <SignInButton />
-    </header>
-  )
+export default function AppBar({ }: Props) {
+    return (
+        <header className='flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow'>
+            <SignInButton />
+        </header>
+    )
 }
